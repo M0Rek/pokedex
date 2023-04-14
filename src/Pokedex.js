9c79b180cc142a02f59b145aeb9cde9a9c4c0634
M0Rek <morek.090@gmail.com>
2023-04-14 20:34:23 +0200

    Add a modal for a PokeCard

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,46 +1,59 @@
-import { Grid } from "@mui/material";
+import { Grid, Modal, Box } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import PokeEntry from "./PokeEntry";
 import Pagination from "./Pagination";
+import PokeCard from "./PokeCard";
 
 export default function Pokedex() {
-    const [data, setData] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-  
-    const nextPage = () => {
-      setCurrentPage(currentPage + 1);
-    };
-  
-    const previousPage = () => {
-      if (currentPage <= 1) {
-        return;
-      }
-      setCurrentPage(currentPage - 1);
-    };
-  
-    useEffect(() => {
-      fetch(
-        "https://pokeapi.co/api/v2/pokemon?limit=20&offset=" +
-          (currentPage - 1) * 20
-      )
-        .then((response) => response.json())
-        .then((data) => setData(data.results));
-    }, [currentPage]);
-  
+  const [open, setOpen] = useState(false);
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
+  const nextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
 
-    return (<>
-        <Grid container justifyContent="center" spacing={4}>
-          {data.map((item) => (
-            <Grid key={item.name} item lg={2} md={3} sm={4} xs={12}>
-              <PokeEntry pokemon={item} />
-            </Grid>
-          ))}
-        </Grid>
-        <Pagination
-          previous={previousPage}
-          page={currentPage}
-          next={nextPage}
-        />
-      </>)
+  const previousPage = () => {
+    if (currentPage <= 1) {
+      return;
+    }
+    setCurrentPage(currentPage - 1);
+  };
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  useEffect(() => {
+    fetch(
+      "https://pokeapi.co/api/v2/pokemon?limit=20&offset=" +
+        (currentPage - 1) * 20
+    )
+      .then((response) => response.json())
+      .then((data) => setData(data.results));
+  }, [currentPage]);
+
+  return (
+    <>
+      <Grid container justifyContent="center" spacing={4}>
+        {data.map((item) => (
+          <Grid key={item.name} item lg={2} md={3} sm={4} xs={12}>
+            <PokeEntry onClick={handleOpen} pokemon={item} />
+          </Grid>
+        ))}
+      </Grid>
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+          }}
+        >
+          <PokeCard />
+        </Box>
+      </Modal>
+      <Pagination previous={previousPage} page={currentPage} next={nextPage} />
+    </>
+  );
 }
