5374bc0a38939b8db885b245d9f403631903ef3f
M0Rek <morek.090@gmail.com>
2023-04-14 20:11:52 +0200

    Add navigation functionality to a navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { AppBar, Toolbar, Button, styled } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const NavButton = styled(Button)({
   fontSize: "1.5rem",
@@ -10,8 +11,12 @@ function Navbar() {
   return (
     <AppBar sx={{ mb: 2 }} position="static">
       <Toolbar>
-        <NavButton color="inherit">Pokedex</NavButton>
-        <NavButton color="inherit">About</NavButton>
+        <NavButton component={Link} to="/" color="inherit">
+          Pokedex
+        </NavButton>
+        <NavButton component={Link} to="/About" color="inherit">
+          About
+        </NavButton>
       </Toolbar>
     </AppBar>
   );
