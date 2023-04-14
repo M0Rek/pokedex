69306a558f534aa71b49618e20f18527fb849e26
M0Rek <morek.090@gmail.com>
2023-04-14 20:11:29 +0200

    Move Pokedex from App to new component

---
+++ src/Pokedex.js
@@ -0,0 +1,46 @@
+import { Grid } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import PokeEntry from "./PokeEntry";
+import Pagination from "./Pagination";
+
+export default function Pokedex() {
+    const [data, setData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+  
+    const nextPage = () => {
+      setCurrentPage(currentPage + 1);
+    };
+  
+    const previousPage = () => {
+      if (currentPage <= 1) {
+        return;
+      }
+      setCurrentPage(currentPage - 1);
+    };
+  
+    useEffect(() => {
+      fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=20&offset=" +
+          (currentPage - 1) * 20
+      )
+        .then((response) => response.json())
+        .then((data) => setData(data.results));
+    }, [currentPage]);
+  
+
+
+    return (<>
+        <Grid container justifyContent="center" spacing={4}>
+          {data.map((item) => (
+            <Grid key={item.name} item lg={2} md={3} sm={4} xs={12}>
+              <PokeEntry pokemon={item} />
+            </Grid>
+          ))}
+        </Grid>
+        <Pagination
+          previous={previousPage}
+          page={currentPage}
+          next={nextPage}
+        />
+      </>)
+}
