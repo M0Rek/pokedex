fb13f3dfad3c3969578bbcf58749926c317e2e79
M0Rek <morek.090@gmail.com>
2023-04-20 09:44:20 +0200

    Style pagination

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,12 +1,21 @@
 import React from "react";
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 
 function Pagination(props) {
   return (
-    <>
-      <Button onClick={props.previous}>Previous</Button>
-      <Button onClick={props.next}>Next</Button>
-    </>
+    <Box display={"flex"} m={2} justifyContent={"center"} width={"fullWidth"}>
+      <Button variant="contained" color="secondary" onClick={props.previous}>
+        Prev
+      </Button>
+      <Button
+        variant="contained"
+        color="secondary"
+        sx={{ marginLeft: "1rem" }}
+        onClick={props.next}
+      >
+        Next
+      </Button>
+    </Box>
   );
 }
 
