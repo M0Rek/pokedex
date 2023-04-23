765fd0aae9e687754f0f66a197663913dc0b4497
M0Rek <morek.090@gmail.com>
2023-04-23 19:21:47 +0200

    Show more stats in PokeCard

--- src/PokeCard.js
+++ src/PokeCard.js
@@ -8,6 +8,9 @@ import {
   Grid,
   Chip,
   Box,
+  Typography,
+  ListItem,
+  List,
 } from "@mui/material";
 import { typeColors } from "./PokeTypeColors";
 import { capitalize } from "./Utilites";
@@ -16,9 +19,9 @@ export default function PokeCard(props) {
   const pokemon = props.pokemon;
 
   return (
-    <Card sx={{ maxHeight: "100vh", maxWidth: "100vw" }}>
+    <Card>
       <CardHeader title={capitalize(pokemon.name)} />
-      <Box sx={{ margin: "16px" }}>
+      <Box sx={{ margin: "8px" }}>
         {pokemon.types.map(({ type }) => (
           <Chip
             key={type.name}
@@ -34,21 +37,41 @@ export default function PokeCard(props) {
       </Box>
       <Grid container>
         <Grid item xs={6}>
-          <CardContent>
+          <CardContent
+            sx={{
+              mb: 2,
+              display: "flex",
+              flexDirection: "column",
+              height: "30vh",
+              overflow: "hidden",
+              overflowY: "scroll",
+            }}
+          >
+            <Typography>
+              <List xs={{ listStyleType: "none", py: 0 }}>
+                <ListItem>Id: {pokemon.id}</ListItem>
+                <ListItem>Height: {pokemon.height}</ListItem>
+                <ListItem>Weight: {pokemon.weight}</ListItem>
+                {pokemon.stats.map((stat) => (
+                  <ListItem>
+                    {stat.stat.name}: {stat.base_stat}
+                  </ListItem>
+                ))}
+              </List>
+            </Typography>
             {console.log(pokemon)}
-            <p>Id: {pokemon.id}</p>
-            <p>
-              Special attack: Special defense: Speed: Base Experience:{" "}
-              {pokemon.base_experience} Height: {pokemon.height}
-            </p>
-            <p>Weight: {pokemon.weight} Abilities: Moves:</p>
           </CardContent>
         </Grid>
         <Grid item xs={6}>
           <CardMedia
             component="img"
             image={pokemon.image}
-            sx={{ objectFit: "contain", maxHeight: "50vh" }}
+            sx={{
+              objectFit: "contain",
+              minHeight: "30vh",
+              maxHeight: "50vh",
+              alignItems: "center",
+            }}
             alt="Image of Pokemon name"
           />
         </Grid>
